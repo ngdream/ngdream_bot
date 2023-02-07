abb8313b092ba4af34b08d56f7af39152677f79a
ngdream <nguewofoss@gmail.com>
2023-02-07 03:49:39 +0100

    production

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -193,16 +193,16 @@ bot.command("share", ctx =>
 
 bot.launch()
   
-  // // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-  // exports.handler = async event => {
-  //   try {
-  //     await bot.handleUpdate(JSON.parse(event.body))
-  //     return { statusCode: 200, body: "" }
-  //   } catch (e) {
-  //     console.error("error in handler:", e)
-  //     return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
-  //   }
-  // }
+  if(process.env.NODE_ENV !== 'production')
+  exports.handler = async event => {
+    try {
+      await bot.handleUpdate(JSON.parse(event.body))
+      return { statusCode: 200, body: "" }
+    } catch (e) {
+      console.error("error in handler:", e)
+      return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+    }
+  }
 
 
 
